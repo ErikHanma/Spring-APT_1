2ca6e317142aae9cd4dca5c36b2a1fdd81397267
Erik <kamaloverik05@mail.ru>
2024-10-22 08:22:36 +0300

    commit

--- src/main/java/com/example/firstproject/FirstProjectApplication.java
+++ src/main/java/com/example/firstproject/FirstProjectApplication.java
@@ -2,7 +2,7 @@ package com.example.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//урааа
 @SpringBootApplication
 public class FirstProjectApplication {
 
